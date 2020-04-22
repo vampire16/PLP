ad4c14dc5db410f51e844d514b7222b13e4c482f
vampire16 <hakevaibhav16@gmail.com>
2020-04-22 09:11:09 +0530

    ADDED : Test method to check owner got a message or not

--- src/test/java/ParkingLotProblemTestCases.java
+++ src/test/java/ParkingLotProblemTestCases.java
@@ -1,4 +1,5 @@
 import com.bridgelabz.service.ParkingLotSystem;
+import com.bridgelabz.utility.Owner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -6,11 +7,15 @@ import org.junit.Test;
 public class ParkingLotProblemTestCases {
     ParkingLotSystem parkingLotSystem = null;
     Object car = null;
+    Object bike = null;
+    Object bus =null;
 
     @Before
     public void setUp() throws Exception {
-        parkingLotSystem = new ParkingLotSystem();
+        parkingLotSystem = new ParkingLotSystem(2);
         car = new Object();
+        bike = new Object();
+        bus = new Object();
     }
 
     @Test
@@ -24,5 +29,20 @@ public class ParkingLotProblemTestCases {
         parkingLotSystem.park(car);
         boolean isUnParked = parkingLotSystem.unPark(car);
         Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParkedDifferentVehicle_ShouldReturnTrue() {
+        parkingLotSystem.park(car);
+        boolean isUnParked = parkingLotSystem.unPark(bike);
+        Assert.assertFalse(isUnParked);
+    }
+
+    @Test
+    public void givenVehicles_WhenFull_ShouldInformOwner() {
+        parkingLotSystem.park(car);
+        parkingLotSystem.park(bike);
+        parkingLotSystem.park(bus);
+        Assert.assertEquals("Parking lot is full", Owner.parkingLotInfo);
     }
 }
