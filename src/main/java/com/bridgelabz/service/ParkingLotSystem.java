8e42126c747c09961f16d318586288daa5e6a516
vampire16 <hakevaibhav16@gmail.com>
2020-04-22 08:37:31 +0530

    ADDED : UnPark method

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -2,7 +2,14 @@ package com.bridgelabz.service;
 
 public class ParkingLotSystem {
 
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
+        this.vehicle = vehicle;
         return vehicle != null;
+    }
+
+    public boolean unPark(Object vehicle) {
+        return this.vehicle == vehicle;
     }
 }
